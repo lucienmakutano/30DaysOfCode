81607ad8dab1fd9ee10221b623d3e685cf03af6f
lucien makutano <54644582+tadomikikuto-bit@users.noreply.github.com>
2020-05-06 22:27:28 +0300

    Update trailingZero.py

--- trailingZero.py
+++ trailingZero.py
@@ -4,19 +4,19 @@ def trailingZeros(number) -> int:
 
   for num in range(1, number + 1 ):
     result = result * num
-  
-  while result != 0:
+
+  while result != 0 and result % 10 == 0:
     digit = result % 10
 
-    result /= 10
+    result //= 10
 
     if digit == 0:
       counter += 1
-
+  
   return counter
 
-
 if __name__ == "__main__":
-  print(trailingZeros(5))
-  print(trailingZeros(3))
-  print(trailingZeros(20))
+  print(trailingZeros(5))   # 1
+  print(trailingZeros(3))   # 0
+  print(trailingZeros(20))  # 4
+  print(trailingZeros(100)) # 24
